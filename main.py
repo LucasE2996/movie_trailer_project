8fc9dca2482ec9852e5e2775be532edc1fa3db42
LucasE2996 <moeda.60@gmail.com>
2018-12-29 17:09:54 -0200

    code review update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import movie
 import my_fresh_tomatoes
 
-# Creates a new movie object
+# Toy Story movie: title, storyline, poster image and movie trailer
 toy_story = movie.Movie(
     "Toy Story",
     "A story of a boy and his toys that come to life",
@@ -9,6 +9,7 @@ toy_story = movie.Movie(
     "https://youtu.be/KYz2wyBy3kc"
     )
 
+# The Emperor's New Groove movie: title, storyline, poster image and movie trailer
 emperor = movie.Movie(
     "The Emperor",
     "A story about a emperor and his new jorney",
@@ -16,6 +17,7 @@ emperor = movie.Movie(
     "https://youtu.be/Hjvy8vc39kw"
     )
 
+# Capitain America movie: title, storyline, poster image and movie trailer
 capitain_america = movie.Movie(
     "Captain America",
     "The Captain America's story",
@@ -23,28 +25,30 @@ capitain_america = movie.Movie(
     "https://youtu.be/JerVrbLldXw"
     )
 
+# Mowgli movie: title, storyline, poster image and movie trailer
 mowgli = movie.Movie(
     "Mowgli",
     "A story about a boy that grows in the jungle",
     "https://upload.wikimedia.org/wikipedia/pt/0/07/Mowgli.jpg",
     "https://youtu.be/FB1KTG-O1V0"
-)
+    )
 
+# Krampus movie: title, storyline, poster image and movie trailer
 krampus = movie.Movie(
     "Krampus",
     "A family has no longer its christmas spirit and soon is terrified by a demon that punish"
     + "those who don't give value to the celebration",
     "https://upload.wikimedia.org/wikipedia/en/1/1e/Krampus_poster.jpg",
     "https://youtu.be/h6cVyoMH4QE"
-)
+    )
 
+# Spider-Man movie: title, storyline, poster image and movie trailer
 spider_man = movie.Movie(
     "Spider-Man",
     "The story about a boy that was bitten by a spider and then receives great power",
     "https://upload.wikimedia.org/wikipedia/en/f/f3/Spider-Man2002Poster.jpg",
     "https://youtu.be/O7zvehDxttM"
-)
-
+    )
 
 # The list that contains all movie objects
 movies = [toy_story, emperor, capitain_america, mowgli, krampus, spider_man]
